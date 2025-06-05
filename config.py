55b88495941b26b4c34b882c20bbcc4a57e62589
mayankgarg2572 <14mayankgarg2001@gmail.com>
2025-06-05 18:12:23 +0530

    Removed Data to just focus on functional part as data can be easily downloaded from respective site.

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ Adjust the DATA_DIR constant to point to where your CSV datasets reside.
 from pathlib import Path
 
 # Absolute or relative path to the folder containing all CSV data files
-DATA_DIR = Path(__file__).parent.resolve() / "data"
+DATA_DIR = Path(__file__).parent.resolve() / "__data__"
 
 # Example:
 # If the project root is /home/user/movie_recommender/, then DATA_DIR resolves to
-# /home/user/movie_recommender/data
+# /home/user/movie_recommender/__data__
